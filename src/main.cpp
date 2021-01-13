aaad871918e065ced2eaa4079fba122c6ea46e01
Peter Grajcar <peter.grajcar131@gmail.com>
2021-01-13 21:00:48 +0100

    implement main function
    Signed-off-by: Peter Grajcar <peter.grajcar131@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include "context.hpp"
+#include "parser.hpp"
 
-int main()
+using namespace jawa;
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    if (argc != 2) {
+        std::cerr << "usage: " << argv[0] << " <source file>" << std::endl;
+        return 1;
+    }
+
+    context ctx{};
+
+    FILE *iff = fopen(argv[1], "r");
+    if (!iff) {
+        std::cerr << "could not open file " << argv[1] << std::endl;
+        return 1;
+    }
+
+    auto scn = lexer_init(iff);
+    parser prs(scn, &ctx);
+
+    prs.parse();
+
+    lexer_shutdown(scn);
+
     return 0;
 }
